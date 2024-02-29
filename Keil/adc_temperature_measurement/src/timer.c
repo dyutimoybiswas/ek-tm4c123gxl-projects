cd8557c6f2ebe28e7f1a4c2165ab95e39af5a3b2
Dyutimoy Biswas <64689883+dyutimoybiswas@users.noreply.github.com>
2024-02-29 15:10:35 +0530

    adc_temperature_measurement

--- Keil/adc_temperature_measurement/src/timer.c
+++ Keil/adc_temperature_measurement/src/timer.c
@@ -1,19 +1,16 @@
 #include "adc_temperature.h"
 
-unsigned volatile int seconds = 0;
-unsigned volatile int handler_counter = 0;
-volatile int is_seconds = 0;
-volatile int is_indicator_enabled = 0;
-volatile int flag = 0;
 
-void set_seconds(unsigned int m){
+void set_seconds(unsigned int m)
+{
 	is_seconds = 1;
 	handler_counter = 0;
 	seconds = (unsigned int)((m * SECONDS_CONSTANT) + 1);
 	timer_init();
 }
 
-void timer_init(){
+void timer_init()
+{
 	SYSCTL->RCGCTIMER |= R0;
 	TIMER0->CTL &= ~TAEN;
 	TIMER0->CFG |= CFG_16_BIT;								// 16 bit mode
@@ -27,14 +24,15 @@ void timer_init(){
 	TIMER0->CTL |= TAEN;
 }
 
-void TIMER0A_Handler(){
+void TIMER0A_Handler()
+{
 	handler_counter++;
 	if(is_indicator_enabled)
 	{
 		GPIOF->DATA |= LED_GREEN;
 	}
 	TIMER0->ICR |= TATOCINT;
-	if(((is_seconds) && (3 * seconds <= handler_counter)) || ((is_miliseconds) && (3 * miliseconds <= handler_counter)))
+	if((is_seconds) && (3 * seconds <= handler_counter))
 	{
 		if(is_indicator_enabled)
 		{
@@ -51,20 +49,23 @@ void TIMER0A_Handler(){
 	}
 }		
 
-void use_indicator(){
+void use_indicator()
+{
 	is_indicator_enabled = 1;
 	SYSCTL->RCGCGPIO |= GPIO_PORTF;
 	GPIOF->DIR |= LED_GREEN;
 	GPIOF->DEN |= LED_GREEN;
 }
 
-void disable_indicator(){
+void disable_indicator()
+{
 	is_indicator_enabled = 0;
 	SYSCTL->RCGCGPIO &= ~GPIO_PORTF;
 	GPIOF->DIR &= ~LED_GREEN;
 	GPIOF->DEN &= ~LED_GREEN;
 }
 
-// void HardFault_Handler(){
-// 	//added to prevent going into hardfault during execution
-// }
+void HardFault_Handler()
+{
+ 	//added to prevent going into hardfault during execution
+}
