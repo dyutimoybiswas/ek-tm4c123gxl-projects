34096846fb210c0d5570b856cdfa75152aa6042e
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-03-01 18:20:36 +0530

    pwm_led_fading

---
+++ Keil/pwm_LED_fading/inc/pwm_constants.h
@@ -0,0 +1,20 @@
+#ifndef PWM_CONSTANTS_H
+#define PWM_CONSTANTS_H
+
+#include "TM4C123GH6PM.h"
+
+#define LED_RED     1U << 1
+#define LED_BLUE    1U << 2
+#define LED_GREEN   1U << 3
+#define PWM5_EN     1U << 5
+#define PWM6_EN     1U << 6
+#define PWM7_EN     1U << 7
+
+void red_init(void);
+void HardFault_Handler(void);
+void delay_msec(unsigned int);
+void set_cmp(void);
+
+static unsigned volatile int duty_cycle_r = 0;
+
+#endif
--- Keil/pwm_LED_fading/src/main.c
+++ Keil/pwm_LED_fading/src/main.c
@@ -1,17 +1,19 @@
+#include <stdbool.h>
 #include "pwm_constants.h"
 
-unsigned volatile int duty_cycle_r = 0;
 
-//HardFault_Handler provided to ensure program doesn't stop at HardFault
-void HardFault_Handler(){
-	
+void HardFault_Handler(void)
+{
+	// HardFault_Handler provided to ensure program doesn't stop at HardFault	
 }
 
-void delay_msec(unsigned int n){
+void delay_msec(unsigned int n)
+{
 	for(unsigned volatile int i = 0; i < 1000 * n; i++);
 }
 
-void red_init(){
+void red_init(void)
+{
 	SYSCTL->RCGCPWM = (1U << 1);
 	SYSCTL->RCGC2 = (1U << 5);			
 	GPIOF->AFSEL &= 0;
@@ -23,26 +25,30 @@ void red_init(){
 	SYSCTL->RCC = ((0x01 << 17) | (1U << 20));		// 16MHz/4 = 4MHz selected
 	
 	//M1PWM5
-	PWM1->_2_CTL &= 0;														//to ensure CTL is cleared we use &=
+	PWM1->_2_CTL &= 0;								//to ensure CTL is cleared we use &=
 	PWM1->_2_GENA = 0x08C;
 	PWM1->_2_GENB |= 0x80C;
-	PWM1->_2_LOAD = 400-1;   										// 4MHz/400 = 10 kHz selected i.e. time period = 100 microsec
+	PWM1->_2_LOAD = 400-1;   						// 4MHz/400 = 10 kHz selected i.e. time period = 100 microsec
 	PWM1->_2_CTL = 1U;
 	
 	PWM1->ENABLE = PWM5_EN;
 }
 
-void set_cmp(){
+void set_cmp(void)
+{
 		PWM1->_2_CMPA = duty_cycle_r - 1;
 		PWM1->_2_CMPB = 400 - duty_cycle_r - 1;
 		delay_msec(50);
 }
 
-int main(){
+int main(void)
+{
 	red_init();
-	while(1){
-		//TODO
-		if(duty_cycle_r == 400)duty_cycle_r = 0;
+	while(true){
+		if(duty_cycle_r == 400)
+		{
+			duty_cycle_r = 0;
+		}
 		duty_cycle_r += 10;
 		set_cmp();
 	}
