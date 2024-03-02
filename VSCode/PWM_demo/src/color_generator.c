06972fddb90a2ec01c4ac0c2700f611db96516ac
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-03-02 16:58:20 +0530

    pwm_demo

--- VSCode/PWM_demo/src/color_generator.c
+++ VSCode/PWM_demo/src/color_generator.c
@@ -1,11 +1,9 @@
 #include "color_generator.h"
 
-int main(int argc, char const *argv[])
+int main(void)
 {
     setup();
 
     // test colors with random values for red, green and blue.
     generate_color(204, 253, 51);
-    
-    return 0;
 }
