b8eb3f50e7ccc7ebacda54c08599aaee50c37d10
Dyutimoy Biswas <64689883+dyutimoybiswas@users.noreply.github.com>
2024-03-02 12:33:10 +0530

    rgb_colors

--- Keil/rgb_colors/inc/rgb.h
+++ Keil/rgb_colors/inc/rgb.h
@@ -11,13 +11,6 @@
 #define PWM7_EN     1U << 7
 
 void HardFault_Handler(void);
-
-#if defined(TURQUOISE)
-void set_turquoise(void);
-#elif defined(MAGENTA)
-void set_magenta(void);
-#elif defined(ORANGE)
-void set_orange(void);
-#endif
+void set_color(void);
 
 #endif
