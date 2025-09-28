03a5e575659ddad418139e3a35c1469130490ef3
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2025-09-28 14:05:57 +0530

    added check to see if env var already set

--- VSCode/Debian/set_tivaware_path.sh
+++ VSCode/Debian/set_tivaware_path.sh
@@ -5,8 +5,12 @@ fi
 
 TIVAWARE_PATH_INPUT="$1"
 
-echo "Setting TIVAWARE_PATH in ~/.bashrc..."
-echo "export TIVAWARE_PATH=$TIVAWARE_PATH_INPUT" >> ~/.bashrc
-echo "Reloading ~/.bashrc to apply changes..."
-source ~/.bashrc
-echo "TIVAWARE_PATH is set to: $TIVAWARE_PATH_INPUT"
+if ! grep -q "^export TIVAWARE_PATH=" ~/.bashrc; then
+	echo "Setting TIVAWARE_PATH in ~/.bashrc..."
+	echo "export TIVAWARE_PATH=$TIVAWARE_PATH_INPUT" >> ~/.bashrc
+	echo "Reloading ~/.bashrc to apply changes..."
+	source ~/.bashrc
+	echo "TIVAWARE_PATH is set to: $TIVAWARE_PATH_INPUT"
+else
+	echo "TIVAWARE_PATH is already set in ~/.bashrc."
+fi
